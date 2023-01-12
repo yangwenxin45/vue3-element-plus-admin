23d965c541935bb59482281da5a7be5080356fdd
yangwenxin <yangwenxin45@gmail.com>
2023-01-12 16:25:04 +0800

    项目初始化配置

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,9 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
+  <el-button type="primary">主要按钮</el-button>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+#app {}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>这里是测试文字</h1>
+    <ul>
+      <li>无序列表LI</li>
+      <li>无序列表LI</li>
+    </ul>
   </div>
 </template>
 
